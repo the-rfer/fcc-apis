30f1842d5244c568341f7c17e88fc4c7471f4c32
knc <ruben.fer252@gmail.com>
2024-07-31 18:58:03 +0100

    fixed parsing data errors

--- urlshortener/index.js
+++ urlshortener/index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
